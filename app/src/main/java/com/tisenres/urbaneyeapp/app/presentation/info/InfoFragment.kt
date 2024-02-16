dd00d35e0f1adbf4e4d6a027f6252a9f10aae19e
tisenres <tisenres@gmail.com>
2024-02-17 00:14:15 +0500

    VideoView

--- app/src/main/java/com/tisenres/urbaneyeapp/app/presentation/info/InfoFragment.kt
+++ app/src/main/java/com/tisenres/urbaneyeapp/app/presentation/info/InfoFragment.kt
@@ -1,10 +1,12 @@
 package com.tisenres.urbaneyeapp.app.presentation.info
 
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.tisenres.urbaneyeapp.R
 import com.tisenres.urbaneyeapp.databinding.FragmentInfoBinding
 
 class InfoFragment : Fragment(), IInfoView {
@@ -17,10 +19,32 @@ class InfoFragment : Fragment(), IInfoView {
     }
     
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentInfoBinding.inflate(layoutInflater)
         return binding.root
+    }
+    
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val videoPath = "android.resource://${requireActivity().packageName}/${R.raw.fire_video}"
+        binding.videoView.setVideoURI(Uri.parse(videoPath))
+        binding.videoView.start()
+    }
+    
+    override fun onPause() {
+        super.onPause()
+        if (binding.videoView.isPlaying) {
+            binding.videoView.pause()
+        }
+    }
+    
+    override fun onResume() {
+        super.onResume()
+        if (!binding.videoView.isPlaying) {
+            binding.videoView.start()
+        }
     }
 }
